fb42b3a6a8c1fafc0bbc4212b645ad5b401eefc8
evenwu <evendesign@gmail.com>
2014-02-12 00:52:09 +0800

    實驗結果

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,22 +1,28 @@
 var rotateBox = document.querySelector('.box');
 function handleOrientation(event) {
 
-  var y = event.gamma; // In degree in the range [-90,90]
+  // var x = event.beta;
+  var y = event.gamma; // [-90,90]
+  // var z = event.alpha; // [0, 360]
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  // if ( x >  90) { x =  90; }
+  // if ( x < -90) { x = -90; }
 
   if(window.innerHeight > window.innerWidth){
-
+    // var rotateXdeg = -x / Math.PI;
     var rotateYdeg = -y / Math.PI;
+    // var rotateZdeg = ( z > 180) ? (z - 360) : z ;
+    // var rotateZdeg = z ;
+    //  clickevent = mobilecheck() ? 'touchstart' : 'click';
 
-    var scaleSize = 320 / Math.cos( Math.abs( y/180 * Math.PI ) ) / 320;
-    // var scaleSize = 1;
+    // var scaleYsize = 320 / Math.cos( Math.abs( x/180 * Math.PI ) ) / 320;
+    var scaleXsize = 320 / Math.cos( Math.abs( y/180 * Math.PI ) ) / 320;
 
-    if ( y > 0 ) {
-      // rotateBox.style.webkitTransformOrigin = "0% 50%";
-    } else {
-      // rotateBox.style.webkitTransformOrigin = "100% 50%";
-    }
+    rotateBox.style.webkitTransform = "rotateY(" + rotateYdeg + "deg) scaleX(" + scaleXsize + ")";
+    // rotateBox.style.webkitTransform = "rotateY(" + rotateYdeg + "deg) rotateZ(" + rotateZdeg + "deg) scaleX(" + scaleXsize + ")";
 
-    rotateBox.style.webkitTransform = "rotateY(" + rotateYdeg + "deg) scaleX(" + scaleSize + ")";
     // rotateBox.style.webkitTransform = "scaleX(" + scaleSize + ")";
   }
 }
